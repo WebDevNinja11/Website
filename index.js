084e6a5d37788a92f826094f65ca9d76569e5bcf
The Syob <michnalex3@gmail.com>
2024-08-29 14:01:04 -0400

    upd

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const menu = document.getElementById("Dropdown")
 const active = false;
 const Dropdown = () => {
-   switch(active){
-    case(active == false):
+   
+    if(active == false){
       menu.style.display = `inline-block`
       active = true;
-      case(active == true):
+    }
+      
+     else if(active == true){
       menu.style.display = `none`      
       active = false;
-   }
+      }
 }
